347748d446233f2b09582358ecebf4c5765cac68
JohnHernCode <besthostandseoinfo@gmail.com>
2021-07-22 11:01:56 -0600

    Create BooksList function

---
+++ src/containers/BooksList.js
@@ -0,0 +1,33 @@
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+import Book from '../components/Book';
+
+function BooksList({
+  books,
+}) {
+  return (
+    <section>
+      {
+          books.length > 0
+            ? books.map((book) => (
+              <Book
+                key={book.id}
+                book={book}
+              />
+            ))
+            : <h3 style={{ textAlign: 'center' }}>No books were found!</h3>
+        }
+    </section>
+  );
+}
+
+BooksList.propTypes = {
+  books: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.oneOfType([PropTypes.number, PropTypes.string]).isRequired,
+    title: PropTypes.string.isRequired,
+    category: PropTypes.string.isRequired,
+  })).isRequired,
+};
+
+export default connect(BooksList);
