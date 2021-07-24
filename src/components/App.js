02a67293439d6522d98436cac92180b0de494cc4
JohnHernCode <besthostandseoinfo@gmail.com>
2021-07-24 08:06:12 -0600

    Add header to app

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,22 @@
-import './App.css';
+import classnames from 'classnames';
 import BooksList from '../containers/BooksList';
 import BooksForm from '../containers/BooksForm';
+import Header from '../containers/Header';
+
+import flex from '../base.module.css';
 
 function App() {
   return (
-    <div>
-      <BooksList />
-      <BooksForm />
-    </div>
+      <div>
+        <Header />
+        <div className={classnames(flex.flex, flex.column, flex.alignCenter)}>
+          <div className={flex.container}>
+            <BooksList />
+            <BooksForm />
+          </div>
+        </div>
+      </div>
+  );
   );
 }
 
