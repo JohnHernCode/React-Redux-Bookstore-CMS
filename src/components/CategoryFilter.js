3aeee1053644f17bec51d8c542fccdad9a60707c
JohnHernCode <besthostandseoinfo@gmail.com>
2021-07-24 08:50:03 -0600

    Add style to category filter

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import classnames from 'classnames';
 import PropTypes from 'prop-types';
 import categories from '../categories';
 
+import flex from '../base.module.css';
+import style from './CategoryFilter.module.css';
+
 function CategoryFilter({ handleFilterChange, filter }) {
   return (
-    <div>
+    <div className={classnames(style.outer, flex.flex, flex.justifyEnd)}>
       <select
+        className={flex.select}
         value={filter}
         onChange={({ target: { value } }) => handleFilterChange(value)}
       >
